1c1778f7d587cc6fd3ee155d38f28f86eae936bb
Avraham Robati <robati4014@example.com>
2021-01-05 17:58:25 +0200

    hw6

---
+++ makefile
@@ -0,0 +1,22 @@
+# These are variables. We usually define CC, CCLINK, CFLAGS, OBJS, EXEC and RM
+CC=gcc
+CCLINK=$(CC) # Read variable CC using $(CC)
+CFLAGS=-g -Wall -std=c99
+OBJS=ransom.o
+EXEC=prog.exe
+RM=clean
+
+# The first rule is invoked by default.
+# We can also invoke it (or any other rule) manually with "make all" - 
+
+$(EXEC): $(OBJS)
+	$(CCLINK) $(OBJS) -o $(EXEC)
+
+# Full rule syntax with custom Shell commands 
+ransom.o: ransom.c
+	$(CC) $(CFLAGS) -c ransom.c
+
+
+# We usually have a "clean" rule to delete any automatically generated files
+$(RM):
+	rm -rf*.o*.exe
---
+++ ransom.c
@@ -0,0 +1,154 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+/*that function "list_contains" gose gets magazin
+ and magazin_count and a word from note
+And checks if a word is included in the list of words from the magazine
+If the word is included, we will delete it from the list of words in 
+the magazine and return the truth
+*/
+bool list_contains(char * word, char** list, int count) {
+    for(int i=0;i<count;i++)
+        if(strcmp(word,list[i])==0)
+        {
+        	// if we faind the word we delet it from magazin
+            strcpy(list[i],"");
+            return true;
+        }
+    return false;
+}
+
+/* that function call to "list contains" for all word in note
+ and if it return "false" we print No otherwise we print Yes*/
+void checkMagazine(int magazine_count, char** magazine, int note_count,
+                    char** note) {
+
+    bool result = true;
+    for(int i=0;i<note_count;i++){
+
+        if(list_contains(note[i],magazine,magazine_count) == false){
+
+            result = false;
+            break;
+        }
+    }
+    printf(result == true ? "Yes" : "No");
+}
+
+
+int main()
+{
+    char** mn = split_string(readline());
+
+    char* m_endptr;
+    char* m_str = mn[0];
+    int m = strtol(m_str, &m_endptr, 10);
+
+    if (m_endptr == m_str || *m_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char* n_endptr;
+    char* n_str = mn[1];
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char** magazine_temp = split_string(readline());
+
+    char** magazine = malloc(m * sizeof(char*));
+
+    for (int i = 0; i < m; i++) {
+        char* magazine_item = *(magazine_temp + i);
+
+        *(magazine + i) = magazine_item;
+    }
+
+    int magazine_count = m;
+
+    char** note_temp = split_string(readline());
+
+    char** note = malloc(n * sizeof(char*));
+
+    for (int i = 0; i < n; i++) {
+        char* note_item = *(note_temp + i);
+
+        *(note + i) = note_item;
+    }
+
+    int note_count = n;
+
+    checkMagazine(magazine_count, magazine, note_count, note);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!line) {
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+    } else {
+        data = realloc(data, data_length + 1);
+
+        data[data_length] = '\0';
+    }
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
